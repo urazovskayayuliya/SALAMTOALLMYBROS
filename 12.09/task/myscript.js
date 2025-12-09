57a84dfc3b5c1710dfbeee36ea72b295c477276d
urazovskayayuliya <urazovskayayuliya06@gmail.com>
2025-12-09 11:47:38 +0400

    Update and rename myscript.js to 12.09/task/myscript.js

---
+++ 12.09/task/myscript.js
@@ -0,0 +1,19 @@
+let obj = new Object();
+obj.name='iPhone 17';
+obj.price=4500;
+obj.category='phone';
+
+let obj1 = new Object();
+obj1.name='Samsung S23';
+obj1.price=3500;
+obj1.category='phone';
+
+let banner = document.getElementsByClassName("banner");
+console.log(banner);
+
+
+let entries = Object.entries(obj);
+let entries1 = Object.entries(obj1);
+
+
+
